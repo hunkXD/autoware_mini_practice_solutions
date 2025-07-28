cb2e1c4d6220cd6ebf783e5cce240702123e56a8
Linux User <linuxuser@linux-d2006-11.domenis.local>
2025-07-28 14:05:23 +0300

    step 7 completed

--- nodes/publisher.py
+++ nodes/publisher.py
@@ -3,7 +3,10 @@ import rospy
 from std_msgs.msg import String
 
 rospy.init_node('publisher')
-rate = rospy.Rate(2)
+rate_hz = rospy.get_param("~rate", 1)  # Default to 1Hz if not set
+rate = rospy.Rate(rate_hz)
+# rate = rospy.Rate(2)
+
 pub = rospy.Publisher('/message', String, queue_size=10)
 message = rospy.get_param('~message', 'Hello World!')
 
