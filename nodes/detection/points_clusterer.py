f56f7f023bcccc3ef8b83992b55de18454b15d41
hunkXD <linuxuser@linux-d2006-11.domenis.local>
2025-08-04 14:43:26 +0300

    practice 5 step 5 complete

--- nodes/detection/points_clusterer.py
+++ nodes/detection/points_clusterer.py
@@ -15,14 +15,15 @@ class PointsCluster:
     def __init__(self):
 
         # Reading in the parameter values
-        self.cluster_epsilon = rospy.get_param("/points_clusterer/cluster_epsilon")
-        self.cluster_min_size = rospy.get_param("/points_clusterer/cluster_min_size")
+        self.cluster_epsilon = rospy.get_param("~cluster_epsilon")
+        self.cluster_min_size = rospy.get_param("~cluster_min_size")
 
 
         # Parameters
         self.clusterer = DBSCAN(eps=self.cluster_epsilon, min_samples=self.cluster_min_size)
 
         # Publishers
+        self.cluster_pub = rospy.Publisher('points_clustered', PointCloud2, queue_size=1, tcp_nodelay=True)
 
         # Subscribers
         rospy.Subscriber('points_filtered', PointCloud2, self.points_callback, queue_size=1, buff_size=2 ** 24,
@@ -31,22 +32,42 @@ class PointsCluster:
     def points_callback(self, msg):
         data = numpify(msg)
         points = structured_to_unstructured(data[['x', 'y', 'z']], dtype=np.float32)
-        print('points shape: ', points.shape)
-        rospy.loginfo('points shape: ', points.shape)
 
         try:
-            assert points.shape[0] == self.clusterer.labels_.first[0]
-            self.clusterer.fit_predict(points)
-            print('labels: ', self.clusterer.labels_)
-            rospy.loginfo('labels: ', self.clusterer.labels_)
+            labels = self.clusterer.fit_predict(points)
+            assert points.shape[0] == len(labels)
         except AssertionError as error:
             print(error)
+
+        idx = labels != -1
+
+        clusters = labels[idx]
+        points_clustered = points[idx]
+
+        # concatenate points with labels
+        points_labeled = np.concatenate([points_clustered, clusters[:, np.newaxis]], axis=1)
+
+
+        # convert labelled points to PointCloud2 format
+        data = unstructured_to_structured(points_labeled, dtype=np.dtype([
+            ('x', np.float32),
+            ('y', np.float32),
+            ('z', np.float32),
+            ('label', np.int32)
+        ]))
+
+        # publish clustered points message
+        cluster_msg = msgify(PointCloud2, data)
+        cluster_msg.header.stamp = msg.header.stamp
+        cluster_msg.header.frame_id = msg.header.frame_id
+
+        self.cluster_pub.publish(cluster_msg)
 
 
     def run(self):
         rospy.spin()
 
 if __name__ == '__main__':
-    rospy.init_node('points_cluster', log_level=rospy.DEBUG)
+    rospy.init_node('points_cluster', log_level=rospy.INFO)
     node = PointsCluster()
     node.run()
